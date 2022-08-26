2bf416c2b7f006a65dd499fe6e73f4e8a1aa4b89
jplima <jplimadev@gmail.com>
2022-08-26 17:26:47 -0300

    autoLayoutDone

--- ComprasUSA/ShoppingViewController.swift
+++ ComprasUSA/ShoppingViewController.swift
@@ -1,6 +1,13 @@
 import UIKit
 
 class ShoppingViewController: UIViewController {
+    
+    
+    @IBOutlet weak var TextFildDolar: UITextField!
+    @IBOutlet weak var labelRealDescription: UILabel!
+    @IBOutlet weak var labelReal: UILabel!
+    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
