80fd89c16a82a92dcc953ecdc1d44e8fcf7a79b4
jplima <jplimadev@gmail.com>
2022-09-02 18:28:45 -0300

    wip_TaxCalculator

--- ComprasUSA/TaxesCalculator.swift
+++ ComprasUSA/TaxesCalculator.swift
@@ -3,4 +3,25 @@ import Foundation
 class TaxesCalculator {
     
     static let shared = TaxesCalculator()
+    var dolar: Double = 3.5
+    var iof: Double = 6.38
+    var stateTax: Double = 7.0
+    var shoppingValue: Double = 0
+    
+    var shoppingValueInReal: Double {
+        return shoppingValue * dolar
+    }
+    
+    var stateTaxValue: Double {
+        return shoppingValue * stateTax/100
+    }
+    
+    var iofValue: Double {
+        return (shoppingValue)
+    }
+    
+    private init() {
+        
+    }
+    
 }
